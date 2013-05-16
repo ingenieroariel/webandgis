fea178983ca598c1a8e5f8cbe4bb8c36afbc3ffd
Ronald Silang <ingenieroariel@gmail.com>
2013-05-16 14:00:27 +0800

    Added unicode method to layer object

--- layers/models.py
+++ layers/models.py
@@ -4,3 +4,6 @@ class Layer(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField(null=True, blank=True)
     original = models.FileField(upload_to='uploads', null=True, blank=True, help_text='Zip file with either geotiff and projection or shapefiles and friends')
+
+    def __unicode__(self):
+        return self.name
