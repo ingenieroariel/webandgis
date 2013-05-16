23b2c7dfede46f7776b9bb18a917626ff7efc1fa
Ronald Silang <ingenieroariel@gmail.com>
2013-05-16 13:49:11 +0800

    Added Layer model and registered it in the admin

---
+++ layers/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from layers.models import Layer
+
+admin.site.register(Layer)
--- webandgis/settings.py
+++ webandgis/settings.py
@@ -117,6 +117,8 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'django.contrib.admin',
     'django.contrib.admindocs',
+
+    'layers',
 )
 
 # A sample logging configuration. The only tangible logging
