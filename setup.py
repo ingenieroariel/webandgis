cb4cebb306364d206487b4a0d840443e810b7e44
Ronald Silang <ingenieroariel@gmail.com>
2013-05-16 15:44:13 +0800

    Switched to pinax-theme-bootstrap, in order to make our site prettier

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     install_requires=[
                       'Django',
                       'django-leaflet',
-                      'django-bootstrap',
+                      'pinax-theme-bootstrap',
                    ],
     packages=find_packages(),
     include_package_data = True,
