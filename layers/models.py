3092abd382d58b47a1a54cbf2eaba2ddb5f8b101
Ronald Silang <ingenieroariel@gmail.com>
2013-05-16 17:10:56 +0800

    Added unzipping of shapefile

--- layers/models.py
+++ layers/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 from django.dispatch import receiver
+from django.conf import settings
+import zipfile
+import os, errno
 
 
 class Layer(models.Model):
@@ -13,6 +16,13 @@ class Layer(models.Model):
     def __unicode__(self):
         return self.name
 
+def create_folder(path):
+    try:
+        os.makedirs(path)
+    except OSError as exc: # Python >2.5
+        if exc.errno == errno.EEXIST and os.path.isdir(path):
+            pass
+        else: raise
 
 @receiver(models.signals.pre_save, sender=Layer)
 def layer_handler(sender, instance, *args, **kwargs):
@@ -20,8 +30,25 @@ def layer_handler(sender, instance, *args, **kwargs):
 
        Get the bounding box information and save it with the model
     """
-    # Unzip the file
-    #import ipdb;ipdb.set_trace()
+    zip_path = instance.original.path
+ 
+
+    # Make a folder with the slug name
+    # and create a 'raw' subdirectory to hold the files
+    layer_folder = os.path.join(settings.MEDIA_ROOT, 'layers', instance.slug)
+    create_folder(layer_folder)
+    zip_out = os.path.join(layer_folder, 'raw')
+    create_folder(zip_out)
+
+    # Iterate over the files in the zip and create them in the raw folder.
+    fh = open(zip_path, 'rb')
+    z = zipfile.ZipFile(fh)
+    for name in z.namelist():
+        outfile = open(os.path.join(zip_out, name), 'wb')
+        outfile.write(z.read(name))
+        outfile.close()
+    fh.close()
+ 
     # Check if it is vector or raster
 
     # Use ogr to inspect the file and get the bounding box
