d39066fddae681618f119c42cbc59265b50ae102
Ronald Silang <ingenieroariel@gmail.com>
2013-05-16 13:54:43 +0800

    Enabled file upload, fixes #3

--- layers/models.py
+++ layers/models.py
@@ -3,3 +3,4 @@ from django.db import models
 class Layer(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField(null=True, blank=True)
+    original = models.FileField(upload_to='uploads', null=True, blank=True, help_text='Zip file with either geotiff and projection or shapefiles and friends')
