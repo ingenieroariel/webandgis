bf483f30ccac4e8f1b2f6c3321dc766fc7fe708d
RK Aranas <rk.aranas@gmail.com>
2013-05-21 05:04:24 -0400

    A fix for issue #30

--- layers/models.py
+++ layers/models.py
@@ -50,16 +50,11 @@ def layer_handler(sender, instance, *args, **kwargs):
     create_folder(zip_out)
 
     # Iterate over the files in the zip and create them in the raw folder.
-    split_zip_path = instance.original.path.rsplit('/', 1)
-    zip_path = os.path.join(split_zip_path[0], 'uploads', split_zip_path[1])
-
-    fh = open(zip_path, 'rb')
-    z = zipfile.ZipFile(fh)
+    z = zipfile.ZipFile(instance.original)
     for name in z.namelist():
         outfile = open(os.path.join(zip_out, name), 'wb')
         outfile.write(z.read(name))
         outfile.close()
-    fh.close()
 
     # Check if it is vector or raster
     # if it has a .shp file, it is vector :)
