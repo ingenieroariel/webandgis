52b2801e1bb60f1e53cc2dc1cb009d442c09ec7f
Ariel Núñez <ingenieroariel@gmail.com>
2013-05-27 10:34:57 -0500

    Switched from signing to login to make django test suite happy. See https://code.djangoproject.com/ticket/20114

--- users/urls.py
+++ users/urls.py
@@ -3,8 +3,8 @@ from django.conf.urls import patterns, url
 
 urlpatterns = patterns(
     '',
-    url(r'signin/$', 'django.contrib.auth.views.login',
-        {'template_name': 'users/signin.html'}, name='signin'),
-    url(r'signout/$', 'django.contrib.auth.views.logout',
-        {'next_page': '/'}, name='signout'),
+    url(r'login/$', 'django.contrib.auth.views.login',
+        {'template_name': 'users/login.html'}, name='login'),
+    url(r'logout/$', 'django.contrib.auth.views.logout',
+        {'next_page': '/'}, name='logout'),
 )
